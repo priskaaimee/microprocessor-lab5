db8489b2489bf311d09201fcbbc70b333a08a73b
priskaaimee <147493955+priskaaimee@users.noreply.github.com>
2025-05-05 11:43:31 +0700

    Update spi.h

--- src/spi.h
+++ src/spi.h
@@ -5,9 +5,6 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-static const uint8_t SMILEFACE[8] = {0x3C, 0x42, 0xA5, 0x81, 0xA5, 0x99, 0x42, 0x3C};
-static const uint8_t SADFACE[8]   = {0x3C, 0x42, 0xA5, 0x81, 0x99, 0xA5, 0x42, 0x3C};
-
 void initSPI();
 void spiWriteByte(uint8_t data);
 void write_Max7219(unsigned char address, unsigned char data);
